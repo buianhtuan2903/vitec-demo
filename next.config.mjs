2e660dd1e881e21a019dd264eaf7f3322d775c02
buianhtuan2903 <buianhtuan2903@gmail.com>
2024-03-13 21:35:37 +0700

    build-demo

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/vitec-demo",
-    // output: "export",  // <=== enables static exports
-    // reactStrictMode: true,
+    basePath: "/vitec-demo",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
   };
 
 export default nextConfig;
